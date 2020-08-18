1817beabda611166408b9a481aa25d900c37fc73
Mary Dick <maryclairedick@gmail.com>
2020-08-18 22:25:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
     element_index = 0
     while element_index < src[row_index].count do
       if src[row_index][element_index].is_a? == string
-        combined_string += src[row_index][element]
+        combined_string.join(src[row_index][element])
       end
       row_index += 1
     end
